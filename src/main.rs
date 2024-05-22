5ac2798f1703562663077d8003123818fbd303ca
HatfieldDave <davidhatfieldjr@gmail.com>
2024-05-22 11:04:42 -0400

    macros and functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::io;
+
 fn main() {
-    println!("rust tutorial");
+    let mut mars_weight = calculate_weight_on_mars(100.0);
+    mars_weight = mars_weight * 1000.0;
+    println!("Weight on mars: {}kg", mars_weight);
+}
+
+fn calculate_weight_on_mars(weight: f32) -> f32 {
+    (weight / 9.81) * 3.711
 }
